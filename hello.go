c2fdfc1608eed357017c920c7a0c42bab9cf37dc
Josteinrygg <35727081+Josteinrygg@users.noreply.github.com>
2018-01-23 12:19:40 +0100

    Update hello.go

--- hello.go
+++ hello.go
@@ -6,6 +6,6 @@ func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("Joakim Høiland Schjølberg is in da house")
 	fmt.Println("Jorgen Lefdal says hallo from his mansion")
-	fmt.Println("Do you even study")
+	fmt.Println("Jostein Våga Rygg")
 }
 

f696a038d83abc6d5b2fe39577b6ce475bf490d6
Marius Tjøstheim <matjot@gmail.com>
2018-01-23 12:31:54 +0100

    La til MT

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("Joakim Høiland Schjølberg is in da house")
 	fmt.Println("Jorgen Lefdal says hallo from his mansion")
-	fmt.Println("Jostein Rygg doesn't study")
 	fmt.Println("Jostein Våga Rygg")
-}
+	fmt.Println("Marius J Tjøstheim")
+	}
 

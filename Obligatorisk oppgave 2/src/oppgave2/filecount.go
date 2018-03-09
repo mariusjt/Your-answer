aa8aaf8f9fc1dd17fbbb904ce19356c89bfbe09f
Jostein Rygg <joster17@uia.no>
2018-03-09 12:28:21 +0100

    Counting Runes
    Yay runes finnes

--- Obligatorisk oppgave 2/src/oppgave2/filecount.go
+++ Obligatorisk oppgave 2/src/oppgave2/filecount.go
@@ -4,7 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"bufio"
+	"strings"
+	"os"
+	"go/scanner"
 )
+
+
 
 func main() {
 		content, err := ioutil.ReadFile("text.txt")
@@ -13,4 +19,15 @@ func main() {
 		}
 
 		fmt.Printf("File contents: %s", content)
+
+	file, err := os.Open("text.txt") // For read access.
+	if err != nil {
+		log.Fatal(err)
+		data := make([]byte, 100)
+		count, err := file.Read(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("read %d bytes: %q\n", count, data[:count])
+	}
 }

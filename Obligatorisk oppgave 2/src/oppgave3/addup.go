1f3cd45e27472c33f883cf9cbe4cc44513082156
Marius T <matjot@gmail.com>
2018-03-09 11:07:39 +0100

    Addup.go kode OK
    Mangler feilhåndtering

--- Obligatorisk oppgave 2/src/oppgave3/addup.go
+++ Obligatorisk oppgave 2/src/oppgave3/addup.go
@@ -1,12 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Legg sammen to tall fra terminal og summer i en annen funksjon")
-	fmt.Println(addUp(10, 10))
+	//fmt.Println(addUp(10, 10))
+	c := make(chan int)
+	input1 := os.Args[1]
+	input2 := os.Args[2]
+	tall,_ := strconv.Atoi(input1)
+	tall2,_ := strconv.Atoi(input2)
+
+	go addUp(tall, tall2, c)
+	sum := <-c
+	fmt.Println("Tall mottatt:")
+	fmt.Println(input1, input2)
+	fmt.Println("Sum:")
+	fmt.Println(sum)
 }
 
-func addUp(tall, tall2 int) int{
-	return tall + tall2
+func addUp(tall int, tall2 int, c chan int){
+	sum := tall + tall2
+	c <- sum
 }

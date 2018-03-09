ff1839842b7607a7380992f872d19c78ec609c1e
Jostein Rygg <joster17@uia.no>
2018-03-09 12:50:27 +0100

    hjelpelærer fix

--- Obligatorisk oppgave 2/src/oppgave2/filecount.go
+++ Obligatorisk oppgave 2/src/oppgave2/filecount.go
@@ -4,10 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"bufio"
-	"strings"
 	"os"
-	"go/scanner"
+
+	"bufio"
 )
 
 
@@ -21,12 +20,29 @@ func main() {
 		fmt.Printf("File contents: %s", content)
 
 	file, err := os.Open("text.txt") // For read access.
+	buf := bufio.NewReader(file)
+
+	var m map[rune]int
+
+	for true {
+		x, _, err := buf.ReadRune()
+		if err != nil {
+			break
+		}
+		m[x]++;
+	}
+
+	for key, value := range m {
+		fmt.Println("Key:", key, "Value:", value)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 		data := make([]byte, 100)
 		count, err := file.Read(data)
 		if err != nil {
 			log.Fatal(err)
+			bufio.ScanRunes(file)
 		}
 		fmt.Printf("read %d bytes: %q\n", count, data[:count])
 	}
